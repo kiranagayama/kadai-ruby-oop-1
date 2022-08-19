cd20e4030fbc30fe986176b4944658ea3c1f0e4a
kira.nagayama <kira20011025@gmail.com>
2022-08-19 04:55:51 +0000

    team.rb

---
+++ team.rb
@@ -0,0 +1,34 @@
+class Team
+ 
+ attr_accessor :name, :win, :lose, :draw
+ 
+  def initialize(name, win, lose, draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
+  end
+ 
+ def calc_win_rate
+   return self.win.to_f/(self.win+self.lose)
+ end
+ 
+ def show_team_result()
+  puts "#{self.name}の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は#{self.win.to_f/(self.win+self.lose)}です。"
+ end
+end
+
+giants= Team.new("giants", 67, 45, 8)
+tigers=Team.new("tigers", 60, 53, 7)
+dragons = Team.new("dragons", 60, 55, 5)
+baystars = Team.new("Baystars", 56, 58, 6)
+carp = Team.new("Carp", 52, 56, 12)
+swallows = Team.new("Swallows", 41, 69, 10)
+
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swallows.show_team_result
+
